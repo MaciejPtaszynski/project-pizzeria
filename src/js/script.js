ceecf86ccf893cd3a570693d98c83373e17fcbb2
Maciej Ptaszynski <mpsendit@gmail.com>
2022-03-15 12:37:13 +0100

    add accordion

--- src/js/script.js
+++ src/js/script.js
@@ -64,7 +64,7 @@
       const thisProduct = this;
       /* generate HTMl based on tamplate */
       const generatedHTML =templates.menuProduct(thisProduct.data);
-      console.log(generatedHTML);
+      // console.log(generatedHTML);
       /* create element using utils.createElementFromHtml */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       /* find menu container */ 
@@ -75,18 +75,21 @@
     initAccordion(){
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = generatedHTML;
-
-    /* START: add event listener to clickable trigger on event click */
-    clickableTrigger.addEventListener('click', function(event) {
-      /* prevent default action for event */
-
-      /* find active product (product that has active class) */
-
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-
-      /* toggle active class on thisProduct.element */
-    });
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelectorAll(select.all.menuProductsActive)
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct != null && activeProduct != thisProduct.element){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        
+      });
 
     }
   };
